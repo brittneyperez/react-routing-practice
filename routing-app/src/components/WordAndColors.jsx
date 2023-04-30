1a12010a78c2f1ab76be76ac04fbd9418bfc628a
Brittney Perez <brittney.mp2005@gmail.com>
2023-04-30 18:30:32 -0400

    completed √

--- routing-app/src/components/WordAndColors.jsx
+++ routing-app/src/components/WordAndColors.jsx
@@ -8,10 +8,21 @@ const WordAndColors = () => {
     return (
         <div>
             <h2>Welcome to the Word and Colors page!</h2>
-            <h3 style={{backgroundColor: bgColor, padding:'10px' }}>
+            { isNaN(word) ? 
+                <h3 style={ bgColor ? {backgroundColor: bgColor} : null }>
+                    Your word is: "
+                    <span style={ textColor ? {color: textColor} : null}>
+                        { word }
+                    </span>
+                    "
+                </h3>
+                :
+                <h3>Your word is the number: { word }</h3> }
+            
+            {/* <h3 style={{backgroundColor: bgColor, padding:'10px' }}>
                 Your word is: 
-                "<span style={{color: textColor }}>{ word }</span>"
-            </h3>
+                <span style={{color: textColor }}>{ word }</span>"
+            </h3> */}
         </div>
     )
 }
